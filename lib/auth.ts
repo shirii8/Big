ef18dbc5ce5b485c185e3695c7576246409cf0fd
shirii8 <24me01086@iitbbs.ac.in>
2026-04-09 21:55:29 +0530

    fix: auth.ts and prisma import from lib/db.ts

---
+++ lib/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+import { prisma } from "@/lib/db"
+import authConfig from "@/auth.config"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+})
