2ce1d4f73accdd8f6b185980fbae9506199cee66
shirii8 <24me01086@iitbbs.ac.in>
2026-04-14 18:58:59 +0530

    Changed Sneaker page + added ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,3 @@
-// In your app/layout.tsx, wrap AuthProvider with CartProvider like this:
-// Replace your existing layout with this:
-
 import type { Metadata } from 'next'
 import { Syne, Bebas_Neue, Space_Mono } from 'next/font/google'
 import Script from 'next/script'
@@ -23,13 +20,42 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${syne.variable} ${bebas.variable} ${mono.variable}`}>
+      <head>
+        {/* Meta Pixel NoScript Fallback */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=988911026819470&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body suppressHydrationWarning>
+        {/* 1. Meta Pixel Script */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '988911026819470');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* 2. Other Third-Party Scripts */}
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"
+          strategy="lazyOnload"
+        />
+
         <AuthProvider>
           <CartProvider>
-            <Script
-              src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"
-              strategy="lazyOnload"
-            />
             <Cursor />
             <Nav />
             <main>{children}</main>
