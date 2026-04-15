cce6cd0bc867d3a116291466205c4bb17162c7ae
shirii8 <24me01086@iitbbs.ac.in>
2026-04-16 00:07:14 +0530

    changed auth temp fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@ import authConfig from "@/auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma as any),
   session: { strategy: "jwt" },
   ...authConfig,
 })
